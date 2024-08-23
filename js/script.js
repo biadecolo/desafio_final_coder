7725e3b52d3ed4ab65360177d1695cb70bde3359
biadecolo <biancadecolo@gmail.com>
2024-08-22 21:41:56 -0300

    mudanças de layout

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
 const pecas = [
-    { id: 1, nome: 'Processador Intel Core i7', preco: 1500.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 2, nome: 'Placa-Mãe ASUS Prime', preco: 800.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 3, nome: 'Memória RAM Kingston 16GB', preco: 450.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 4, nome: 'Placa de Vídeo NVIDIA RTX 3080', preco: 4500.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 5, nome: 'SSD Samsung 1TB', preco: 600.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 6, nome: 'Fonte Corsair 750W', preco: 350.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 7, nome: 'Gabinete Cooler Master', preco: 400.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 8, nome: 'Teclado Mecânico Razer', preco: 500.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 9, nome: 'Mouse Gamer Logitech', preco: 250.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 10, nome: 'Monitor LG 24"', preco: 900.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 11, nome: 'Processador AMD Ryzen 9', preco: 1800.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 12, nome: 'Placa-Mãe MSI MAG', preco: 750.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 13, nome: 'Memória RAM Corsair 32GB', preco: 950.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 14, nome: 'Placa de Vídeo AMD Radeon RX 6800', preco: 3200.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 15, nome: 'HD Seagate 2TB', preco: 350.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 16, nome: 'Cooler Master Hyper 212', preco: 200.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 17, nome: 'Gabinete NZXT H510', preco: 650.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 18, nome: 'Teclado Mecânico HyperX', preco: 450.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 19, nome: 'Mouse Gamer Razer', preco: 300.00, imgUrl: 'https://picsum.photos/100/100' },
-    { id: 20, nome: 'Monitor Dell 27"', preco: 1300.00, imgUrl: 'https://picsum.photos/100/100' },
+  
+  { id: 1, nome: 'O Poderoso Chefão', preco: 50.00, imgUrl: 'img/godfather.png' },
+  { id: 2, nome: 'A Origem', preco: 50.00, imgUrl: 'img/inception.png' },
+  { id: 3, nome: 'Interestelar', preco: 50.00, imgUrl: 'img/interestelar.png' },
+  { id: 4, nome: 'Vingadores: Ultimato', preco: 50.00, imgUrl: 'img/avengers.png' },
+  { id: 5, nome: 'Parasita', preco: 50.00, imgUrl: 'img/parasita.png' },
+  { id: 6, nome: 'Coringa', preco: 50.00, imgUrl: 'img/joker.png' },
+  { id: 7, nome: 'Matrix', preco: 50.00, imgUrl: 'img/matrix.png' },
+  { id: 8, nome: 'Gladiador', preco: 50.00, imgUrl: 'img/gladiador.png' },
+  { id: 9, nome: 'O Senhor dos Anéis: O Retorno do Rei', preco: 50.00, imgUrl: 'img/senhoraneis.png' },
+  { id: 10, nome: 'Pulp Fiction', preco: 50.00, imgUrl: 'img/pulpf.png' },
+  { id: 11, nome: 'Clube da Luta', preco: 50.00, imgUrl: 'img/fightclub.png' },
+  { id: 12, nome: 'O Cavaleiro das Trevas', preco: 50.00, imgUrl: 'img/batmanruim.png' },
+  { id: 13, nome: 'Forrest Gump', preco: 50.00, imgUrl: 'img/forrestgump.png' },
+  { id: 14, nome: 'A Lista de Schindler', preco: 50.00, imgUrl: 'img/schindler.png' },
+  { id: 15, nome: 'O Iluminado', preco: 50.00, imgUrl: 'img/shining.png' },
+  { id: 16, nome: 'Blade Runner 2049', preco: 50.00, imgUrl: 'img/blade.png' },
+  { id: 17, nome: 'Django Livre', preco: 50.00, imgUrl: 'img/django.png' },
+  { id: 18, nome: 'Mad Max: Estrada da Fúria', preco: 50.00, imgUrl: 'img/madmax.png' },
+  { id: 19, nome: 'O Grande Lebowski', preco: 50.00, imgUrl: 'img/lebowski.png' },
+  { id: 20, nome: 'A Vida é Bela', preco: 50.00, imgUrl: 'img/avidaehbela.png' },
  
   ];
   
@@ -29,12 +30,16 @@ const pecas = [
 
   pecas.forEach((e)=>{  
     acumular += `
-    <div>
-        <img src=${e.imgUrl} alt=${e.nome}/>
-       <p style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">${e.nome}</p>
-       <p>R$${e.preco.toFixed(2)}</p>
-       <button>Comprar</button>
-        </div>
+      <div class="col-md-3 cardprod">
+          <div class="card" style="">
+            <img class="card-img-top" src="${e.imgUrl}" alt="${e.nome}">
+            <div class="card-body">
+              <h5 class="card-title tailwind-truncate">${e.nome}</h5>
+              <p class="card-text">R$${e.preco.toFixed(2)}</p>
+              <a href="#" class="btn btn-primary">Quero alugar</a>
+            </div>
+          </div>
+      </div>
     `
   })
   
